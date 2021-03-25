4798d218b09ab6c692d1b56891563072f4d91d6a
shivaramkothapally <shivaramkothapally177@gmail.com>
2021-03-24 20:52:53 -0500

    commit 2

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var c =0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+   c = c+1;
+  res.send('User accesses are: '+c);
 });
 
 module.exports = router;
